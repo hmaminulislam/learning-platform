f67c31ad97e63dfe8d57119c1c963981954fc1b4
Aminul <mdaminulislamsahin14@gmail.com>
2022-10-25 08:28:09 +0600

    server basic setup done

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const app = express();
 const port = process.env.port || 5000
 
 app.use(cors());
-
+const hotels = './data/hotel.json'
 
 app.get('/', (req, res) => {
     res.send('Study bd server is running...')
+})
+
+app.get('/hotels', (req, res) => {
+  res.send(hotels)
 })
 
 app.listen(port, () => {
